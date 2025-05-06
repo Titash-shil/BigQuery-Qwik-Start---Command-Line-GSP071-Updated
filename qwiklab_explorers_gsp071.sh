86fde5c9f2a7ae67892e2f6842a8b68b2eca5490
QwikLab Explorers <155870248+Titash-shil@users.noreply.github.com>
2025-05-06 12:55:01 +0530

    Update qwiklab_explorers_gsp071.sh

--- qwiklab_explorers_gsp071.sh
+++ qwiklab_explorers_gsp071.sh
@@ -1,7 +1,23 @@
+#!/bin/bash
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+clear
+
 
 bq show bigquery-public-data:samples.shakespeare
 echo 
 
+echo "${YELLOW_TEXT}${BOLD_TEXT} Searching for words containing 'raisin' in Shakespeare's works...${RESET_FORMAT}"
 bq query --use_legacy_sql=false \
 'SELECT
   word,
@@ -23,10 +39,12 @@ bq query --use_legacy_sql=false \
   word = "huzzah"'
 echo 
 
+echo "${MAGENTA_TEXT}${BOLD_TEXT} Creating a new BigQuery dataset named 'babynames'...${RESET_FORMAT}"
 bq mk babynames
 echo 
 
-wget https://github.com/Titash-shil/BigQuery-Qwik-Start---Command-Line-GSP071-Updated/blob/main/names.zip
+echo "${MAGENTA_TEXT}${BOLD_TEXT} Downloading the baby names data archive...${RESET_FORMAT}"
+wget https://github.com/ArcadeCrew/Google-Cloud-Labs/raw/refs/heads/main/BigQuery%20Qwik%20Start%20-%20Command%20Line/names.zip
 echo 
 
 unzip names.zip
@@ -41,9 +59,15 @@ echo
 bq query "SELECT name,count FROM babynames.names2010 WHERE gender = 'M' ORDER BY count ASC LIMIT 5"
 echo 
 
+echo "${RED_TEXT}${BOLD_TEXT} Removing the 'babynames' dataset to clean up resources...${RESET_FORMAT}"
 bq rm -r babynames
 echo 
 
+echo "${RED_TEXT}${BOLD_TEXT} Deleting the downloaded and extracted local files...${RESET_FORMAT}"
 rm -f names.zip yob2010.txt
 echo 
 
+echo
+echo "${MAGENTA_TEXT}${BOLD_TEXT} Subscribe to QwikLab Explorers ${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@qwiklabexplorers${RESET_FORMAT}"
+echo
