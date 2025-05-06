f36ef9456e4697cab70d4d58f194de0089b4ef80
QwikLab Explorers <155870248+Titash-shil@users.noreply.github.com>
2025-05-06 12:44:24 +0530

    Create qwiklab_explorers_gsp071.sh

---
+++ qwiklab_explorers_gsp071.sh
@@ -0,0 +1,49 @@
+
+bq show bigquery-public-data:samples.shakespeare
+echo 
+
+bq query --use_legacy_sql=false \
+'SELECT
+  word,
+  SUM(word_count) AS count
+ FROM
+  `bigquery-public-data`.samples.shakespeare
+ WHERE
+  word LIKE "%raisin%"
+ GROUP BY
+  word'
+echo 
+
+bq query --use_legacy_sql=false \
+'SELECT
+  word
+ FROM
+  `bigquery-public-data`.samples.shakespeare
+ WHERE
+  word = "huzzah"'
+echo 
+
+bq mk babynames
+echo 
+
+wget https://github.com/ArcadeCrew/Google-Cloud-Labs/raw/refs/heads/main/BigQuery%20Qwik%20Start%20-%20Command%20Line/names.zip
+echo 
+
+unzip names.zip
+echo 
+
+bq load babynames.names2010 yob2010.txt name:string,gender:string,count:integer
+echo 
+
+bq query "SELECT name,count FROM babynames.names2010 WHERE gender = 'F' ORDER BY count DESC LIMIT 5"
+echo 
+
+bq query "SELECT name,count FROM babynames.names2010 WHERE gender = 'M' ORDER BY count ASC LIMIT 5"
+echo 
+
+bq rm -r babynames
+echo 
+
+rm -f names.zip yob2010.txt
+echo 
+
